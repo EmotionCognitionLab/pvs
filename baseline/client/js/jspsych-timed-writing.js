c4617852ed5ee41ff177d5874b84e89b5c7c9fc7
Floozutter <me@floozutter.site>
2021-05-12 14:18:13 -0700

    add jspsych-timed-writing.js plugin

---
+++ baseline/client/js/jspsych-timed-writing.js
@@ -0,0 +1,65 @@
+jsPsych.plugins["timed-writing"] = (() => {
+    const plugin = {};
+
+    plugin.info = {
+        name: "timed-writing",
+        parameters: {
+            duration: {
+                type: jsPsych.plugins.parameterType.INT,
+                default: undefined,
+            },
+            stimulus: {
+                type: jsPsych.plugins.parameterType.HTML_STRING,
+                default: undefined,
+            },
+            textarea_rows: {
+                type: jsPsych.plugins.parameterType.INT,
+                default: undefined,
+            },
+            textarea_cols: {
+                type: jsPsych.plugins.parameterType.INT,
+                default: undefined,
+            },
+            show_timer: {
+                type: jsPsych.plugins.parameterType.BOOL,
+                default: true,
+            }
+        },
+    };
+
+    plugin.trial = (display_element, trial) => {
+        // build and show display HTML
+        {
+            let html = "";
+            html += `<div id="jspsych-timed-writing-timer"></div>`;
+            html += `<div id="jspsych-timed-writing-stimulus">${trial.stimulus}</div>`;
+            const [r, c] = [trial.textarea_rows, trial.textarea_cols];
+            html += `<textarea id="jspsych-timed-writing-textarea" rows="${r}" cols="${c}"></textarea>`;
+            display_element.innerHTML = html;
+            document.querySelector("#jspsych-timed-writing-timer").hidden = !trial.show_timer;
+        }
+        // start display timer to show remaining time
+        {
+            let clock_seconds = Math.floor(trial.duration / 1000) - 1;
+            (function update_clock() {
+                const clock = document.querySelector("#jspsych-timed-writing-timer");
+                if (clock !== null && clock_seconds >= 0) {
+                    clock.textContent = `${clock_seconds} s`;
+                    --clock_seconds;
+                    setTimeout(update_clock, 1000);
+                }
+            })();
+        }
+        // start logical timer to end trial
+        setTimeout(() => {
+            const data = {
+                stimulus: trial.stimulus,
+                response: document.querySelector("#jspsych-timed-writing-textarea").value,
+            };
+            display_element.innerHTML = "";
+            jsPsych.finishTrial(data);
+        }, trial.duration);
+    };
+
+    return plugin;
+})();
