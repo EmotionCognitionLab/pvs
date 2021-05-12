6e70fc8fc476e082b2f41d4b0683f1d6638e1f3e
Floozutter <me@floozutter.site>
2021-05-12 14:19:39 -0700

    start verbal-fluency

---
+++ baseline/client/verbal-fluency/verbal-fluency.js
@@ -0,0 +1,18 @@
+import "@adp-psych/jspsych/jspsych.js";
+import "js/jspsych-timed-writing.js";
+import "@adp-psych/jspsych/css/jspsych.css";
+
+const test = {
+    type: "timed-writing",
+    duration: 60000,
+    stimulus: "uwu",
+    textarea_rows: 5,
+    textarea_cols: 40,
+};
+
+jsPsych.init({
+    timeline: [
+        test,
+    ],
+    on_finish: () => { jsPsych.data.displayData("json"); },
+});
