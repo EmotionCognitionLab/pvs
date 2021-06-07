4a0e553228e16896ec138b0f89c6bf22520caa3f
Noah Mercer <noah_ten@yahoo.com>
2021-06-07 11:43:10 -0700

    First step of client application

---
+++ client/README.md
@@ -0,0 +1,29 @@
+# HeartBeam
+
+An application (based on [Vue](vuejs.org) and [Electron](electronjs.org)) that provides an alternative interface to [emWave](https://store.heartmath.com/emwavepro) for heartbeat data from the emWave sensor and that guides participants in the HeartBeam study through a daily series of tasks.
+
+If you don't already have them, you will need [node](nodejs.org) and [yarn](https://yarnpkg.com) installed. Once you have those, you can use the commands below to set up and run the project.
+
+## Project setup
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+```
+yarn electron:serve
+```
+
+### Compiles and minifies for production
+TODO: Replace with electron-forge documentation
+```
+yarn build
+```
+
+### Lints and fixes files
+```
+yarn lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ client/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <TimerComponent v-bind:secondsDuration=300 @timerStarted="handleTimerStart" @timerStopped="handleTimerStop"></TimerComponent>
+  <br/>
+  <em-wave-listener ref="emWaveListener"></em-wave-listener>
+</template>
+
+<script>
+import TimerComponent from './components/TimerComponent.vue'
+import EmWaveListener from './components/EmWaveListener.vue'
+import { ref } from '@vue/runtime-core'
+
+export default {
+  name: 'App',
+  components: {
+    EmWaveListener,
+    TimerComponent
+  },
+  setup() {
+    const emWaveListener = ref(null)
+    const handleTimerStart = () => {
+      emWaveListener.value.startPulseSensor()
+    }
+    const handleTimerStop = () => {
+      emWaveListener.value.stopPulseSensor()
+    }
+
+    return {emWaveListener, handleTimerStart, handleTimerStop}
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
