1b59a3e57a2e4b339b09891dadeeeb561563b23f
Noah Mercer <noah_ten@yahoo.com>
2021-06-18 16:01:14 -0700

    Removes range key on users table. It complicated fetches by userId

--- server/main.tf
+++ server/main.tf
@@ -112,15 +112,9 @@ resource "aws_dynamodb_table" "users-table" {
   read_capacity  = 1
   write_capacity = 1
   hash_key       = "userId"
-  range_key      = "createdAt"
 
   attribute {
     name = "userId"
-    type = "S"
-  }
-
-  attribute {
-    name = "createdAt"
     type = "S"
   }
 }
