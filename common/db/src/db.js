bdfc0a0cbd59a33ab71cc2698cfb9703445b882b
Noah Mercer <noah_ten@yahoo.com>
2021-06-25 11:55:12 -0700

    Adds function to fetch all experimental results for logged-in user

--- common/db/src/db.js
+++ common/db/src/db.js
@@ -9,17 +9,8 @@
  'use strict';
   
 function saveResults(session, experiment, results) {
-    const idToken = session.getIdToken().getJwtToken();
-    const payload = idToken.split('.')[1];
-    const tokenobj = JSON.parse(atob(payload));
-    const subId = tokenobj['sub'];
-
-    const credentials = new AWS.CognitoIdentityCredentials({
-        IdentityPoolId: awsSettings.IdentityPoolId,
-        Logins: {
-            [`cognito-idp.${awsSettings.AWSRegion}.amazonaws.com/${awsSettings.UserPoolId}`]: idToken
-        }
-    }, {region: awsSettings.AWSRegion});
+    const subId = getSubIdFromSession(session);
+    const credentials = getCredentialsForSession(session);
 
     credentials.refresh(async err => {
         if (err) {
@@ -27,7 +18,7 @@ function saveResults(session, experiment, results) {
         }
         const docClient = new DynamoDB.DocumentClient({region: awsSettings.AWSRegion, credentials: credentials});
         const params = {
-            TableName: 'pvs-dev-experiment-data', // TODO exeternalize this
+            TableName: awsSettings.ExperimentTable,
             Item: {
                 userExperimentDateTime: `${subId}|${experiment}|${new Date().toISOString()}`,
                 identityId: credentials.identityId,
@@ -38,4 +29,54 @@ function saveResults(session, experiment, results) {
     });
 }
 
-export { saveResults }
+async function getAllResultsForCurrentUser(session) {
+    const credentials = getCredentialsForSession(session);
+
+    try {
+        await credentials.refreshPromise();
+        const docClient = new DynamoDB.DocumentClient({region: awsSettings.AWSRegion, credentials: credentials});
+        const params = {
+            TableName: awsSettings.ExperimentTable,
+            KeyConditionExpression: `identityId = :idKey`,
+            ExpressionAttributeValues: { ':idKey': credentials.identityId }
+        };
+        const dynResults = await docClient.query(params).promise();
+        const results = dynResults.Items.map(i => {
+            const parts = i.userExperimentDateTime.split('|');
+            if (parts.length != 3) {
+                throw new Error(`Unexpected userExperimentDateTime value: ${i.userExperimentDateTime}. Expected three parts, but found ${parts.length}.`)
+            }
+            const experiment = parts[1];
+            const dateTime = parts[2];
+            return {
+                experiment: experiment,
+                dateTime: dateTime,
+                results: i.results
+            }
+        });
+        return results;
+    } catch (err) {
+        throw new Error('Error refreshing credentials while fetching exeperiment results', err);
+    }
+}
+
+function getCredentialsForSession(session) {
+    const idToken = session.getIdToken().getJwtToken();
+    const credentials = new AWS.CognitoIdentityCredentials({
+        IdentityPoolId: awsSettings.IdentityPoolId,
+        Logins: {
+            [`cognito-idp.${awsSettings.AWSRegion}.amazonaws.com/${awsSettings.UserPoolId}`]: idToken
+        }
+    }, {region: awsSettings.AWSRegion});
+    
+    return credentials;
+}
+
+function getSubIdFromSession(session) {
+    const idToken = session.getIdToken().getJwtToken();
+    const payload = idToken.split('.')[1];
+    const tokenobj = JSON.parse(atob(payload));
+    return tokenobj['sub'];
+}
+
+export { saveResults, getAllResultsForCurrentUser }
