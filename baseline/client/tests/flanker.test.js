319b8af6bcadab622e342344b896720cc795810e
Noah Mercer <noah_ten@yahoo.com>
2021-07-21 20:41:05 -0400

    Updates test to match flanker changes

--- baseline/client/tests/flanker.test.js
+++ baseline/client/tests/flanker.test.js
@@ -2,27 +2,31 @@ require("@adp-psych/jspsych/jspsych.js");
 import { Flanker } from "../flanker/flanker.js";
 import { pressKey } from "./utils.js"
 
+afterEach(() => {
+    jest.useRealTimers();
+});
+
 describe("flanker", () => {
     it("results should have at least one result marked isRelevant", () => {
+        jest.useFakeTimers();
         let timeline = (new Flanker(3)).getTimeline();
         // drop the preload; the test env doesn't get past it
         timeline = timeline.slice(1);
-        expect(timeline.length).toBe(6);
+        expect(timeline.length).toBeGreaterThanOrEqual(3);
+
         jsPsych.init({timeline: timeline});
 
         // welcome screen -> instruction
         pressKey(" ");
-        // instruction screen -> trial
+        // instruction -> fixation 1
         pressKey(" ");
-        // first trial
-        pressKey("f");
-        // second trial
-        pressKey("f");
-        // third trial
-        pressKey("f");
-
+        // fixation 1 -> trial 1
+        jest.advanceTimersByTime(800);
+        // trial 1 -> fixation 2
+        pressKey("ArrowLeft");
+        
         // check the data
         const relevantData = jsPsych.data.get().filter({isRelevant: true}).values();
-        expect(relevantData.length).toBe(3);
+        expect(relevantData.length).toBeGreaterThanOrEqual(1);
     });
 });
