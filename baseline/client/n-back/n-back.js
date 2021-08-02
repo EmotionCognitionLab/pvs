9ac184ab8ff9b11e891d8f77d203553f83eee564
Floozutter <me@floozutter.site>
2021-08-02 16:25:49 -0700

    generate sequences randomly from constructor seed

--- baseline/client/n-back/n-back.js
+++ baseline/client/n-back/n-back.js
@@ -1,6 +1,7 @@
 import "@adp-psych/jspsych/jspsych.js";
 import "@adp-psych/jspsych/plugins/jspsych-html-keyboard-response.js";
 import "js/jspsych-n-back.js";
+import { Random, MersenneTwister19937 } from "random-js";
 import "@adp-psych/jspsych/css/jspsych.css";
 import "css/jspsych-n-back.css";
 import "./style.css";
@@ -13,27 +14,63 @@ import rest_html from "./frag/rest.html";
 import completion_html from "./frag/completion.html";
 
 export class NBack {
+    constructor(setNum, seed = null) {
+        this.setNum = setNum;
+        if (seed === null) {
+            this.random = new Random(MersenneTwister19937.autoSeed());
+        } else if (Number.isInteger(seed)) {
+            this.random = new Random(MersenneTwister19937.seed(seed));
+        } else {
+            throw new Error("seed must be null or an integer");
+        }
+    }
+
     getTimeline() {
         return [
             this.constructor.introduction,
             this.constructor.instruction(instruction_html),
             this.constructor.instruction(instruction_0_html),
             this.constructor.cue,
-            this.constructor.trial(0, ["5", "6", "3", "6", "1", "7", "7"]),
+            this.randTrial(0),
             this.constructor.rest,
             this.constructor.instruction(instruction_1_html),
             this.constructor.cue,
-            this.constructor.trial(1, ["5", "6", "3", "6", "1", "7", "7"]),
+            this.randTrial(1),
             this.constructor.rest,
             this.constructor.instruction(instruction_2_html),
             this.constructor.cue,
-            this.constructor.trial(2, ["5", "6", "3", "6", "1", "7", "7"]),
+            this.randTrial(2),
             this.constructor.completion,
         ];
     }
 
     get taskName() {
         return this.constructor.taskName;
+    }
+
+    randTrial(n) {
+        return {
+            type: "n-back",
+            n: n,
+            sequence: this.randSequence(
+                ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
+                15,
+                n,
+                4
+            ),
+            show_duration: 400,
+            hide_duration: 1400,
+            data: { isRelevant: true },
+        }
+    }
+
+    randSequence(choices, length, n, targets) {
+        while (true) {
+            const sequence = Array(length).fill().map(_ => this.random.pick(choices));
+            if (this.constructor.countTargets(n, sequence) === targets) {
+                return sequence;
+            }
+        }
     }
 }
 
@@ -64,20 +101,17 @@ NBack.cue = {
     trial_duration: 2000,
 };
 
-NBack.trial = (n, sequence) => ({
-    type: "n-back",
-    n: n,
-    sequence: sequence,
-    show_duration: 400,
-    hide_duration: 1400,
-    data: { isRelevant: true },
-});
-
 NBack.rest = {
     type: "html-keyboard-response",
     stimulus: rest_html,
     choices: jsPsych.NO_KEYS,
     trial_duration: 10000,
+};
+
+NBack.countTargets = (n, sequence) => {
+    return sequence.filter((item, index) => (
+        n === 0 ? item === "1" : item === sequence[index - n]
+    )).length;
 };
 
 
