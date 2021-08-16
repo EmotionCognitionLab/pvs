8efe6c6f86064ddb767ad0e6f3a64c9819f29b2f
Floozutter <me@floozutter.site>
2021-08-16 11:07:09 -0700

    start jspsych-spatial-orientation.js

---
+++ baseline/client/js/jspsych-spatial-orientation.js
@@ -0,0 +1,12 @@
+jsPsych.plugins["spatial-orientation"] = (() => {
+    const plugin = {};
+
+    plugin.info = {
+        name: "spatial-orientation",
+        parameters: {},
+    };
+
+    plugin.trial = (display_element, trial) => {};
+
+    return plugin;
+})();
