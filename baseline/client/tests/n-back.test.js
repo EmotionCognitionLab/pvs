59b0278a4f63b65f05f468eee3e7c586f1c7a9a6
Floozutter <me@floozutter.site>
2021-10-19 16:42:03 -0700

    add and use completeCurrentTrial

--- baseline/client/tests/n-back.test.js
+++ baseline/client/tests/n-back.test.js
@@ -1,20 +1,56 @@
 import { NBack } from "../n-back/n-back.js";
 import { pressKey, cartesianProduct } from "./utils.js"
 
+const pressBubblingKey = key => {
+    const initDict = {key: key, bubbles: true};
+    const display = document.querySelector(".jspsych-display-element");
+    display.dispatchEvent(new KeyboardEvent("keydown", initDict));
+    display.dispatchEvent(new KeyboardEvent("keyup", initDict));
+};
+
+const completeCurrentTrial = correctly => {
+    const trial = jsPsych.currentTrial();
+    const progress = jsPsych.progress();
+    if (trial.type === "html-keyboard-response") {
+        if (typeof trial.trial_duration === "number") {
+            jest.advanceTimersByTime(trial.trial_duration);
+        } else {
+            pressKey(trial.choices[0]);
+        }
+    } else if (trial.type === "n-back") {
+        trial.sequence.forEach((x, i) => {
+            if (correctly && (trial.n === 0 ? x === "1" : x === trial.sequence[i - trial.n])) {
+                pressBubblingKey(" ");
+            }
+            jest.advanceTimersByTime(trial.show_duration + trial.hide_duration);
+        });
+    }
+    // assert that progress occurred
+    if (jsPsych.progress().current_trial_global <= progress.current_trial_global) {
+        throw Error("progress didn't increase");
+    }
+};
+
 describe("n-back", () => {
     it("results should have at least one result marked isRelevant", () => {
         // check timeline nodes
-        const timeline = (new NBack(2)).getTimeline();
+        const timeline = (new NBack(1)).getTimeline();
         expect(timeline.some(trial => trial.data && trial.data.isRelevant)).toBe(true);
         // check generated data
         jest.useFakeTimers("legacy");
-        jsPsych.init({timeline: timeline});
-        timeline.forEach(() => {
-            jest.runAllTimers();
-            pressKey(" "); pressKey("0");
+        let complete = false;
+        jsPsych.init({
+            timeline: timeline,
+            on_finish: () => { complete = true; },
         });
-        const progress = jsPsych.progress();
-        expect(progress.current_trial_global).toBe(progress.total_trials);
+        let trials = 0;
+        while (!complete) {
+            completeCurrentTrial(true);
+            ++trials;
+            if (trials > 100) {
+                throw new Error("why");
+            }
+        }
         const relevant = jsPsych.data.get().filter({isRelevant: true}).values();
         expect(relevant.length).toBeGreaterThan(0);
     });
