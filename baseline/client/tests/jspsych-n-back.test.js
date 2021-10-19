4026c85bdc64ec42fdd3b646e484d26b79667b8d
Floozutter <me@floozutter.site>
2021-10-18 18:20:01 -0700

    extend missing indices test with mocked keypresses

--- baseline/client/tests/jspsych-n-back.test.js
+++ baseline/client/tests/jspsych-n-back.test.js
@@ -1,5 +1,6 @@
 import "@adp-psych/jspsych/jspsych.js";
 import "js/jspsych-n-back.js";
+import { pressKey } from "./utils.js"
 
 describe("jspsych-n-back.js plugin", () => {
     it("loads correctly", () => {
@@ -7,16 +8,32 @@ describe("jspsych-n-back.js plugin", () => {
     });
 
     it("finds missed indices correctly", () => {
-        jest.useFakeTimers("legacy");
-        jsPsych.init({timeline: [{
+        const testSettings = n => ({timeline: [{
             type: "n-back",
-            n: 0,
-            sequence: [0, 0, 1, 1, 0, 1].map(String),
-            show_duration: 0,
+            n: n,
+            sequence: [0, 0, 1, 1, 0, 1, 0].map(String),
+            show_duration: 1000,
             hide_duration: 0,
         }]});
+        const getLastTrialData = () => jsPsych.data.getLastTrialData().values()[0];
+        jest.useFakeTimers("legacy");
+        // all missed
+        jsPsych.init(testSettings(0));
         jest.runAllTimers();
-        const data = jsPsych.data.getLastTrialData().values()[0];
-        expect(data.missedIndices).toStrictEqual([2, 3, 5]);
+        expect(getLastTrialData().missedIndices).toStrictEqual([2, 3, 5]);
+        // none missed
+        jsPsych.init(testSettings(1));
+        jest.advanceTimersByTime(1000);
+        pressKey(" ");
+        jest.advanceTimersByTime(2000);
+        pressKey(" ");
+        jest.runAllTimers();
+        expect(getLastTrialData().missedIndices).toStrictEqual([]);
+        // one missed
+        jsPsych.init(testSettings(2));
+        jest.advanceTimersByTime(5000);
+        pressKey(" ");
+        jest.runAllTimers();
+        expect(getLastTrialData().missedIndices).toStrictEqual([6]);
     });
 });
