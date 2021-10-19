02816426084a8cd62ac4375e3fe3f0e5cae89472
Floozutter <me@floozutter.site>
2021-10-18 18:28:43 -0700

    bubble dispatched KeyboardEvents

--- baseline/client/tests/jspsych-n-back.test.js
+++ baseline/client/tests/jspsych-n-back.test.js
@@ -2,6 +2,13 @@ import "@adp-psych/jspsych/jspsych.js";
 import "js/jspsych-n-back.js";
 import { pressKey } from "./utils.js"
 
+const pressBubblingKey = key => {
+    const initDict = {key: key, bubbles: true};
+    const display = document.querySelector(".jspsych-display-element");
+    display.dispatchEvent(new KeyboardEvent("keydown", initDict));
+    display.dispatchEvent(new KeyboardEvent("keyup", initDict));
+};
+
 describe("jspsych-n-back.js plugin", () => {
     it("loads correctly", () => {
         expect(jsPsych.plugins["n-back"]).toBeDefined();
@@ -24,15 +31,15 @@ describe("jspsych-n-back.js plugin", () => {
         // none missed
         jsPsych.init(testSettings(1));
         jest.advanceTimersByTime(1000);
-        pressKey(" ");
+        pressBubblingKey(" ");
         jest.advanceTimersByTime(2000);
-        pressKey(" ");
+        pressBubblingKey(" ");
         jest.runAllTimers();
         expect(getLastTrialData().missedIndices).toStrictEqual([]);
         // one missed
         jsPsych.init(testSettings(2));
         jest.advanceTimersByTime(5000);
-        pressKey(" ");
+        pressBubblingKey(" ");
         jest.runAllTimers();
         expect(getLastTrialData().missedIndices).toStrictEqual([6]);
     });
