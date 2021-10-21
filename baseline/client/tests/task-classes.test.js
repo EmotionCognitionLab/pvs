c1a2b12d8bf660fdd11ae427e929fb6df4e9699f
Noah Mercer <noah_ten@yahoo.com>
2021-10-20 22:00:41 -0700

    Updates task classes test to include all task classes, give more descriptive messages

--- baseline/client/tests/task-classes.test.js
+++ baseline/client/tests/task-classes.test.js
@@ -1,33 +1,48 @@
 import { DailyStressors } from "../daily-stressors/daily-stressors.js";
+import { Dass } from "../dass/dass.js";
+import { Demographics } from "../demographics/demographics.js";
+import { FaceName } from "../face-name/face-name.js";
+import { Ffmq } from "../ffmq/ffmq.js";
 import { Flanker } from "../flanker/flanker.js";
+import { MindEyes } from "../mind-eyes/mind-eyes.js";
 import { MoodMemory } from "../mood-memory/mood-memory.js";
 import { MoodPrediction } from "../mood-prediction/mood-prediction.js";
 import { NBack } from "../n-back/n-back.js";
 import { Panas } from "../panas/panas.js";
+import { PatternSeparation } from "../pattern-separation/pattern-separation.js";
+import { PhysicalActivity } from "../physical-activity/physical-activity.js";
+import { SpatialOrientation } from "../spatial-orientation/spatial-orientation.js";
+import { TaskSwitching } from "../task-switching/task-switching.js";
 import { VerbalFluency } from "../verbal-fluency/verbal-fluency.js";
 import { VerbalLearning } from "../verbal-learning/verbal-learning.js";
 
-describe("task classes", () => {
-    const classes = [
-        DailyStressors,
-        Flanker,
-        MoodMemory,
-        MoodPrediction,
-        NBack,
-        Panas,
-        VerbalFluency,
-        VerbalLearning,
-    ];
+describe.each([
+    [ "DailyStressors", DailyStressors ],
+    [ "Dass", Dass ],
+    [ "Demographics", Demographics ],
+    [ "FaceName", FaceName ],
+    [ "Ffmq", Ffmq ],
+    [ "Flanker", Flanker ],
+    [ "MindEyes", MindEyes ],
+    [ "MoodMemory", MoodMemory ],
+    [ "MoodPrediction", MoodPrediction ],
+    [ "NBack", NBack ],
+    [ "Panas", Panas ],
+    [ "PatternSeparation", PatternSeparation ],
+    [ "PhysicalActivity", PhysicalActivity ],
+    [ "SpatialOrientation", SpatialOrientation ],
+    [ "TaskSwitching", TaskSwitching ],
+    [ "VerbalFluency", VerbalFluency ],
+    [ "VerbalLearning", VerbalLearning ],
+])("%s", (name, taskClass) => {
     
-    it("each has taskName", () => {
-        classes.forEach(c => {
-            expect(typeof c.taskName).toBe("string");
-        });
+    it(`${name} has taskName`, () => {
+        expect(typeof taskClass.taskName).toBe("string");
+        const inst = new taskClass(1);
+        expect(typeof inst.taskName).toBe("string");
     });
     
-    it("each has getTimeline", () => {
-        classes.forEach(c => {
-            expect(typeof c.prototype.getTimeline).toBe("function");
-        });
+    it(`${name} has getTimeline`, () => {
+        expect(typeof taskClass.prototype.getTimeline).toBe("function");
     });
 });
