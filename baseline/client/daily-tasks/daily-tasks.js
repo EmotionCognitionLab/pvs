EmotionCognitionLab/pvs

commit 67727a0d1cea57c17dbe7ad3fabe01c399dcb0c4

#68 changes rules for when users can do next set

baseline/client/daily-tasks/daily-tasks.js
@@ -72,14 +72,13 @@ function getSetAndTasks(allResults, saveResultsCallback) {
                 const setNum = i + 1; // add 1 b/c setNum starts from 1
                 if (j === 0 && !nextSetOk) {
                     // we're at the start of a new set and the participant
-                    // started their most recent set today
-                    // participants can only start one set per day
+                    // doesn't meet the criteria for starting the next one yet
                     return { set: setNum, remainingTasks: [{timeline: [doneForTodayMessage], taskName: doneForToday}] };
                 }
                 // they didn't finish this set - return the remaining tasks
                 remainingTasks = set.slice(j)
                 const timeline = tasksForSet(remainingTasks, setNum, allResults, saveResultsCallback, nextSetOk);
-                if (j > 0 && nextSetOk) {
+                if (j > 0 && nextSetOk && i < allSets.length - 2) {
                     timeline.push({timeline: startNewSetQueryTask, taskName: startNewSetQuery}); // give them the choice to start the next set
                     Array.prototype.push.apply(timeline, tasksForSet(allSets[i+1], setNum + 1, allResults, saveResultsCallback, false));
                 }
@@ -232,21 +231,36 @@ async function verbalLearningEndTime() {
 
 
 /**
- * Users may only start the next set if (a) they started the previous one yesterday or earlier and (b) at least one hour ago.
- * Example: If it is Tuesday at 12:17AM and the user started the previous set Monday at 11:43PM they can't start the next set.
+ * Users may only start the next set if:
+ * (a) They are in between sets and
+ *    (1) They completed the last set >= 1 hour ago or
+ *    (2) The last set took them >3 hours to complete
+ * or
+ * (b) They are in the middle of a set that they started >3 hours ago
+ * See https://github.com/EmotionCognitionLab/pvs/issues/68
  * @param {Object[]} allResults All results for the user, as returned by ../common/db/db.js:getAllResultsForCurrentUser()
  * @returns true if the user can start the next set, false otherwise
  */
 function canStartNextSet(allResults) {
-    const mostRecentStart = allResults.filter(r => r.experiment === setStarted).reverse()[0];
-    if (!mostRecentStart) {
-        return true;
+    if (allResults.length === 0) return false;
+
+    const setStarts = allResults.filter(r => r.experiment === setStarted);
+    const setFinishes = allResults.filter(r => r.experiment === setFinished);
+    const setNumsStarted = new Set(setStarts.map(ss => ss.results.setNum));
+    const setNumsFinished = new Set(setFinishes.map(sf => sf.results.setNum));
+    let inBetweenSets = true;
+    for (let setNum of setNumsStarted) if (!setNumsFinished.has(setNum)) inBetweenSets = false;
+    inBetweenSets = inBetweenSets && (setNumsStarted.size == setNumsFinished.size);
+
+    if (inBetweenSets) {
+        const lastSetStartedAt = Date.parse(setStarts[setStarts.length - 1].dateTime);
+        const lastSetFinishedAt = Date.parse(setFinishes[setFinishes.length - 1].dateTime);
+        return Date.now() - lastSetFinishedAt >= 1 * 60 * 60 * 1000 ||
+            (lastSetFinishedAt - lastSetStartedAt > 3 * 60 * 60 * 1000);
+    } else {
+        const lastSetStartedAt = Date.parse(setStarts[setStarts.length - 1].dateTime);
+        return Date.now() - lastSetStartedAt > 3 * 60 * 60 * 1000;
     }
-    const lastSetStart = new Date(mostRecentStart.dateTime);
-    const now = Date.now();
-    const yesterday = new Date(now - (1000 * 60 * 60 * 24));
-    return lastSetStart < yesterday || 
-        (lastSetStart.getDate() === yesterday.getDate() && lastSetStart.valueOf() <= now - (1000 * 60 * 60));
 }
 
 function init() {

baseline/client/tests/daily-tasks.test.js
@@ -21,7 +21,8 @@ require("@adp-psych/jspsych/jspsych.js");
 
 describe("getSetAndTasks", () => {
     it("returns the set and remaining tasks in the set", () => {
-        const input = buildInput(dailyTasks.allSets[0].slice(0, 2));
+
+        const input = buildInput( [{ taskNames: dailyTasks.allSets[0].slice(0, 2), setNum: 1 }] );
         const result = dailyTasks.getSetAndTasks(input);
         expect(result.set).toBe(1);
         const expectedTaskNames = dailyTasks.allSets[0].slice(input.length - 1); // -1 because the input includes a set-started record
@@ -31,8 +32,15 @@ describe("getSetAndTasks", () => {
         expect(remainingTaskNames).toStrictEqual(expectedTaskNames);
     });
 
-    it("returns the next set and all tasks in it if all tasks in the previous set have been completed and the previous set was started yesterday or earlier", () => {
-        const input = buildInput(dailyTasks.allSets[0], '2021-01-01T12:34:56.789Z');
+    it("returns the next set and all tasks in it if all tasks in the previous set have been completed and the previous set was finished more than one hour ago", () => {
+        const now = Date.now();
+        const moreThanAnHourAgo = new Date(now - ((1000 * 60 * 60 * 1) + 1));
+        const input = buildInput( [{ 
+            taskNames: dailyTasks.allSets[0], 
+            setStartedTime: new Date(now - (1000 * 60 * 60 * 1.8)).toISOString(),
+            setFinishedTime: moreThanAnHourAgo.toISOString(),
+            setNum: 1
+        }]);
         const result = dailyTasks.getSetAndTasks(input);
         expect(result.set).toBe(2);
         const expectedTaskNames = dailyTasks.allSets[result.set - 1];
@@ -42,18 +50,44 @@ describe("getSetAndTasks", () => {
         expect(remainingTaskNames).toStrictEqual(expectedTaskNames);
     });
 
-    it("returns an 'all done for today' message if all tasks in the previous set were completed today", () => {
-        const input = buildInput(dailyTasks.allSets[0], new Date().toISOString());
+    it("returns an 'all done for today' message if all tasks in the previous set were completed less than an hour ago and the set took less than 3 hours to complete", () => {
+        const now = Date.now();
+        const lessThanAnHourAgo = new Date(now - (1000 * 60 * 60 * 0.5));
+        const input = buildInput( [{
+            taskNames: dailyTasks.allSets[0], 
+            setStartedTime: new Date(now - (1000 * 60 * 60 * 1)).toISOString(),
+            setFinishedTime: lessThanAnHourAgo.toISOString(),
+            setNum: 1
+        }]);
         const results = dailyTasks.getSetAndTasks(input);
         const remainingTaskNames = results.remainingTasks.map(t => t.taskName);
         expect(remainingTaskNames).toStrictEqual([dailyTasks.doneForToday]);
     });
 
-    it("gives you the option to start a new set if you're finishing a set that you started yesterday", () => {
-        const yesterdayMs = Date.now() - (1000 * 60 * 60 * 24);
-        const yesterday = new Date(yesterdayMs);
+    it("lets you start the next set if the previous set took you more than three hours to complete, even if you just finished it", () => {
+        const now = Date.now();
+        const moreThanThreeHoursAgo = new Date(now - 1000 * 60 * 60 * 3.01);
+        const input = buildInput( [{
+            taskNames: dailyTasks.allSets[0],
+            setStartedTime: moreThanThreeHoursAgo.toISOString(),
+            setFinishedTime: new Date(now).toISOString(),
+            setNum: 1
+        }]);
+        const results = dailyTasks.getSetAndTasks(input);
+        const remainingTaskNames = results.remainingTasks.map(t => t.taskName);
+        const secondSetTasks = dailyTasks.allSets[1];
+        expect(remainingTaskNames).toEqual(expect.arrayContaining(secondSetTasks));
+    });
+
+    it("gives you the option to start a new set if you're finishing a set that you started more than three hours ago", () => {
+        const fourHoursAgo = new Date(Date.now() - (1000 * 60 * 60 * 4));
         const doneTasksIdx = 3;
-        const input = buildInput(dailyTasks.allSets[0].slice(0, doneTasksIdx), yesterday.toISOString());
+        const input = buildInput( [{
+            taskNames: dailyTasks.allSets[0].slice(0, doneTasksIdx), 
+            setStartedTime: fourHoursAgo.toISOString(),
+            setFinishedTime: new Date(Date.now() - (1000 * 60 * 60 * 3.1)).toISOString(),
+            setNum: 1
+        }]);
         const results = dailyTasks.getSetAndTasks(input);
         const remainingTaskNames = results.remainingTasks.map(t => t.taskName);
         expect(remainingTaskNames).toContain(dailyTasks.startNewSetQuery);
@@ -63,18 +97,23 @@ describe("getSetAndTasks", () => {
         expect(remainingTaskNames).toEqual(expect.arrayContaining(secondSetTasks));
     });
 
-    it("does not give you the option to start a new set if you finished the last one yesterday but less than an hour ago", () => {
-        const lastSetFinishedDate = '2021-01-02T23:32:00.000Z';
-        const nowDateMs = Date.parse(lastSetFinishedDate).valueOf() + (1000 * 60 * 40);
-        const input = buildInput(dailyTasks.allSets[0], lastSetFinishedDate);
-        jest.spyOn(global.Date, 'now').mockImplementationOnce(() => nowDateMs);
+    it("does not give you the option to start a new set if you're finishing a set that you started less than three hours ago", () => {
+        const twoHoursAgo = new Date(Date.now() - (1000 * 60 * 60 * 2));
+        const doneTasksIdx = 4;
+        const input = buildInput( [{
+            taskNames: dailyTasks.allSets[0].slice(0, doneTasksIdx),
+            setStartedTime: twoHoursAgo,
+            setNum: 1
+        }]);
         const results = dailyTasks.getSetAndTasks(input);
-        expect(results.remainingTasks.length).toEqual(1);
-        expect(results.remainingTasks.map(t => t.taskName)[0]).toEqual(dailyTasks.doneForToday);
+        const remainingTaskNames = results.remainingTasks.map(t => t.taskName);
+        expect(remainingTaskNames).not.toContain(dailyTasks.startNewSetQuery);
+        const remainingFirstSetTasks = dailyTasks.allSets[0].slice(doneTasksIdx);
+        expect(remainingTaskNames).toEqual(expect.arrayContaining(remainingFirstSetTasks));
     });
 
     it("throws an error if completed tasks are not in the expected order", () => {
-        const input = buildInput([dailyTasks.allSets[0][0], dailyTasks.allSets[0][2]]);
+        const input = buildInput([{ taskNames: [dailyTasks.allSets[0][0], dailyTasks.allSets[0][2]], setNum: 1 }]);
         function callWithBadOrder() {
             dailyTasks.getSetAndTasks(input);
         }
@@ -83,7 +122,14 @@ describe("getSetAndTasks", () => {
     });
 
     it("should include an 'all-done' message (and only that message) if the user has completed all tasks in all sets", () => {
-        const input = buildInput(dailyTasks.allSets.flatMap(s => s));
+        const setList = dailyTasks.allSets.map( (s, idx) => ( { 
+            taskNames: s,
+            setStartedTime: new Date(Date.now() - (1000 * 60 * 60 * 2)).toISOString(),
+            setFinishedTime: new Date(Date.now() - (1000 * 60 * 60 * 1)).toISOString(),
+            setNum: idx + 1 }
+        ));
+        
+        const input = buildInput(setList);
         const result = dailyTasks.getSetAndTasks(input);
         expect(result.remainingTasks.length).toBe(1);
         expect(result.remainingTasks[0].taskName).toBe(dailyTasks.allDone);
@@ -94,7 +140,7 @@ describe("getSetAndTasks", () => {
         const input = [];
         inputTasks.forEach(t => { input.push(t); input.push(t) } );
         expect(input.length).toBe(2 * inputTasks.length);
-        const result = dailyTasks.getSetAndTasks(buildInput(input));
+        const result = dailyTasks.getSetAndTasks(buildInput( [{taskNames: input, setNum: 1}] ));
         expect(result.set).toBe(1);
         const expectedTaskNames = dailyTasks.allSets[result.set - 1].slice(5);
         const remainingTaskNames = result.remainingTasks
@@ -104,10 +150,22 @@ describe("getSetAndTasks", () => {
     });
 
     it("should return remaining tasks when the number of characters in the first uncompleted task name is less than or equal to the number of completed tasks", () => {
-        let inputTasks = dailyTasks.allSets[0].concat(dailyTasks.allSets[1].slice(0, dailyTasks.allSets[1].length - 2));
+        const inputSets = [ {
+            taskNames: dailyTasks.allSets[0],
+            setNum: 1,
+            setStartedTime: new Date(Date.now() - (1000 * 60 * 60 * 5)).toISOString(),
+            setFinishedTime: new Date(Date.now() - (1000 * 60 * 60 * 4)).toISOString()
+        },
+        {
+            taskNames: dailyTasks.allSets[1].slice(0, dailyTasks.allSets[1].length - 2),
+            setNum: 2,
+            setStartedTime: new Date(Date.now() - (1000 * 60 * 60 * 2)).toISOString(),
+        }
+        ];
+        
         const expectedTaskNames = dailyTasks.allSets[1].slice(dailyTasks.allSets[1].length - 2);
-        expect(expectedTaskNames[0].length).toBeLessThanOrEqual(inputTasks.length);
-        const input = buildInput(inputTasks);
+        expect(expectedTaskNames[0].length).toBeLessThanOrEqual(inputSets[0].taskNames.concat(inputSets[1].taskNames).length);
+        const input = buildInput(inputSets);
         const result = dailyTasks.getSetAndTasks(input);
         const remainingTaskNames = result.remainingTasks
             .filter(t => t.taskName !== dailyTasks.doneForToday)
@@ -116,20 +174,26 @@ describe("getSetAndTasks", () => {
     });
 
     it("should include a 'done-for-today' message after the user finishes the last task for the day", () => {
-        const input = buildInput(dailyTasks.allSets[0].slice(0, 2));
+        const input = buildInput( [{ taskNames: dailyTasks.allSets[0].slice(0, 2), setNum: 1 }] );
         const result = dailyTasks.getSetAndTasks(input);
         expect(result.remainingTasks[result.remainingTasks.length -1].taskName).toBe(dailyTasks.doneForToday);
     });
 
     it("should include an 'all-done' message when the user finishes the last task of the last set", () => {
-        let input = dailyTasks.allSets.flatMap(s => s);
-        input = input.slice(0, input.length - 2);
-        const result = dailyTasks.getSetAndTasks(buildInput(input));
+        const setList = dailyTasks.allSets.map( (s, idx) => ( { 
+            taskNames: s,
+            setStartedTime: new Date(Date.now() - (1000 * 60 * 60 * 2)).toISOString(),
+            setFinishedTime: new Date(Date.now() - (1000 * 60 * 60 * 1)).toISOString(),
+            setNum: idx + 1 }
+        ));
+        const lastSetTasks = setList[setList.length - 1].taskNames
+        setList[setList.length - 1].taskNames = lastSetTasks.slice(0, lastSetTasks.length - 2);
+        const result = dailyTasks.getSetAndTasks(buildInput(setList));
         expect(result.remainingTasks[result.remainingTasks.length - 1].taskName).toBe(dailyTasks.allDone);
     });
 
     it("should return all of the tasks the first set if you have started the set today but have not yet done any tasks", () => {
-        const result = dailyTasks.getSetAndTasks(buildInput([]));
+        const result = dailyTasks.getSetAndTasks(buildInput([ { taskNames: [], setNum: 1 } ]));
         const remainingTaskNames = result.remainingTasks
             .filter(t => t.taskName !== dailyTasks.doneForToday)
             .map(t => t.taskName);
@@ -394,8 +458,24 @@ describe("doing the tasks", () => {
     });
 });
 
-function buildInput(taskList, dateTime = new Date().toISOString()) {
-    const input = taskList.map(task => ( {experiment: task} ));
-    input.unshift({experiment: dailyTasks.setStarted, dateTime: dateTime});
-    return input;
+/**
+ * 
+ * @param {*} setList Array of objects {
+ *  taskNames: ["first task", "second task",...], 
+ *  setStartedTime: ISO time string, (optional, defaults to now)
+ *  setFinishedTime: ISO time string, (optional)
+ *  setNum: number
+ * }
+ */
+function buildInput(setList) {
+    return setList.flatMap(s => {
+        const startTime = s.setStartedTime || (new Date()).toISOString();
+        let input = 
+            [{experiment: dailyTasks.setStarted, dateTime: startTime, results: {setNum: s.setNum}}]
+            .concat(s.taskNames.map(task => ({experiment: task})))
+        if (s.setFinishedTime) {
+            input = input.concat({experiment: dailyTasks.setFinished, dateTime: s.setFinishedTime, results: {setNum: s.setNum}});
+        }
+        return input;
+    });
 }

commit 5d8a316e202081aaa23248ffcab49ea607d1ba08

Fixes bug introduced in 67727a0d1cea57c .

baseline/client/daily-tasks/daily-tasks.js
@@ -78,7 +78,7 @@ function getSetAndTasks(allResults, saveResultsCallback) {
                 // they didn't finish this set - return the remaining tasks
                 remainingTasks = set.slice(j)
                 const timeline = tasksForSet(remainingTasks, setNum, allResults, saveResultsCallback, nextSetOk);
-                if (j > 0 && nextSetOk && i < allSets.length - 2) {
+                if (j > 0 && nextSetOk && i < allSets.length - 1) {
                     timeline.push({timeline: startNewSetQueryTask, taskName: startNewSetQuery}); // give them the choice to start the next set
                     Array.prototype.push.apply(timeline, tasksForSet(allSets[i+1], setNum + 1, allResults, saveResultsCallback, false));
                 }