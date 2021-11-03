cbf0ee12d7d3392f36320d20c2fae398e3fdb948
Floozutter <me@floozutter.site>
2021-11-03 15:07:25 -0700

    expect it shows stimulus

--- baseline/client/tests/jspsych-memory-field.test.js
+++ baseline/client/tests/jspsych-memory-field.test.js
@@ -5,4 +5,16 @@ describe("jspsych-memory-field.js plugin", () => {
     it("loads correctly", () => {
         expect(jsPsych.plugins["memory-field"]).toBeDefined();
     });
+
+    it("shows stimulus", () => {
+        const stimulus = "const stimulus";
+        const buttonLabel = "const buttonLabel";
+        jsPsych.init({timeline: [{
+            type: "memory-field",
+            stimulus: stimulus,
+            button_label: buttonLabel,
+        }]});
+        expect(jsPsych.getDisplayElement().innerHTML).toContain(stimulus);
+        expect(jsPsych.getDisplayElement().innerHTML).toContain(buttonLabel);
+    });
 });
