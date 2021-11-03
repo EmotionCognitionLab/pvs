f65d22e399a77bfe6d825c0a983fde7ab7a5d2d7
Floozutter <me@floozutter.site>
2021-11-03 14:47:26 -0700

    expect it shows stimulus

--- baseline/client/tests/jspsych-timed-writing.test.js
+++ baseline/client/tests/jspsych-timed-writing.test.js
@@ -5,4 +5,16 @@ describe("jspsych-timed-writing.js plugin", () => {
     it("loads correctly", () => {
         expect(jsPsych.plugins["timed-writing"]).toBeDefined();
     });
+
+    it("shows stimulus", () => {
+        const stimulus = "trust rock";
+        jsPsych.init({timeline: [{
+            type: "timed-writing",
+            duration: 1000,
+            stimulus: stimulus,
+            textarea_rows: 1,
+            textarea_cols: 1,
+        }]});
+        expect(jsPsych.getDisplayElement().innerHTML).toContain(stimulus);
+    });
 });
