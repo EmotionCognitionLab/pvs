ca2cdf58728306d93a0c4cd8085e12d399b86d65
Floozutter <me@floozutter.site>
2021-11-12 15:58:29 -0800

    allow tests to start at main trial with any letter

--- baseline/client/tests/verbal-fluency.test.js
+++ baseline/client/tests/verbal-fluency.test.js
@@ -2,19 +2,17 @@ require("@adp-psych/jspsych/jspsych.js");
 import { VerbalFluency } from "../verbal-fluency/verbal-fluency.js";
 import { pressKey } from "./utils.js";
 
-jest.useFakeTimers('legacy'); // why legacy: https://github.com/facebook/jest/issues/11500
-
-beforeEach(() => {
-    let timeline = (new VerbalFluency(VerbalFluency.possibleLetters[0])).getTimeline();
-    expect(timeline.length).toBe(2);
-    jsPsych.init({timeline: timeline});
+const startAtTimedWriting = letter => {
+    jsPsych.init({timeline: (new VerbalFluency(letter)).getTimeline()});
     // instruction screen -> trial
     pressKey(" ");
-});
+};
 
 describe("verbal-fluency", () => {
     it("results should have at least one result marked isRelevant", () => {
-        // first trial
+        jest.useFakeTimers("legacy");  // why legacy: https://github.com/facebook/jest/issues/11500
+        startAtTimedWriting("f");
+        // timed writing trial
         const inputField = document.querySelector("#jspsych-timed-writing-textarea");
         inputField.value = "fee fi fo fum";
         // let's not wait 60 seconds for the trial to finish
@@ -25,7 +23,9 @@ describe("verbal-fluency", () => {
     });
 
     it("should give the user 60 seconds to generate words", () => {
-        //expect(setTimeout).toHaveBeenCalledTimes(1);
+        jest.useFakeTimers("legacy");
+        startAtTimedWriting(VerbalFluency.possibleLetters[0]);
+        //expect(setTimeout).toHaveBeenCalledTimes(1);  // display timer prevents this
         expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 60000);
     });
 });
