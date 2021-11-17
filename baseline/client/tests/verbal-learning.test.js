000f44e1127c90d5409a0286db8506d31515d9d4
Floozutter <me@floozutter.site>
2021-11-17 03:49:08 -0800

    test recorded data by mocking preload and a-k-r
    preload and audio-keyboard-response are jsPsych plugins that are
    annoying to use in Jest tests

--- baseline/client/tests/verbal-learning.test.js
+++ baseline/client/tests/verbal-learning.test.js
@@ -1,12 +1,55 @@
-require("@adp-psych/jspsych/jspsych.js");
 import { VerbalLearning } from "../verbal-learning/verbal-learning.js";
+import instruction_check_loop_html from "../verbal-learning/frag/instruction_check_loop.html";
+import { pressKey } from "./utils.js";
+
+// mock away annoying plugins with a dummy
+const dummyPlugin = {
+    info: {name: "dummy", parameters: {}},
+    trial: () => { jsPsych.finishTrial({}); },
+};
+jsPsych.plugins["preload"] = dummyPlugin;
+jsPsych.plugins["audio-keyboard-response"] = dummyPlugin;
+
+const completeCurrentTrial = () => {
+    const trial = jsPsych.currentTrial();
+    const progress = jsPsych.progress();
+    if (trial.type === "html-keyboard-response") {
+        if (typeof trial.trial_duration === "number") {
+            jest.advanceTimersByTime(trial.trial_duration);
+        } else {
+            pressKey(trial.choices[0]);
+        }
+    } else if (trial.type === "html-button-response") {
+        // click "Sound Worked Fine"
+        jsPsych.getDisplayElement().querySelectorAll("button")[1].click();
+    } else if (trial.type === "audio-keyboard-response") {
+        // audio plugin is mocked to finish trial immediately
+    } else if (trial.type === "countdown") {
+        jest.advanceTimersByTime(trial.duration);
+    } else if (trial.type === "memory-field") {
+        document.getElementById("jspsych-memory-field-button").click();
+    }
+    // assert that progress occurred
+    if (jsPsych.progress().current_trial_global <= progress.current_trial_global) {
+        console.log(trial);
+        throw Error("progress didn't increase");
+    }
+};
+
+beforeEach(() => {
+    jest.useFakeTimers("legacy");
+});
+
+afterEach(() => {
+    jest.useRealTimers();
+});
 
 describe("verbal-learning", () => {
     it("results should have at least one result marked isRelevant", () => {
-        const timeline = [
-            {timeline: new VerbalLearning(1, 1).getTimeline()},
-            {timeline: new VerbalLearning(1, 2, () => Date.now()).getTimeline()},
-        ];
+        // get timelines for segments 1 and 2
+        const timeline1 = (new VerbalLearning(1, 1)).getTimeline();
+        const timeline2 = (new VerbalLearning(1, 2, () => Date.now())).getTimeline();
+        // test timeline nodes for relevant data
         const timelineHasRelevantData = timeline => {
             for (const node of timeline) {
                 if (node.timeline === undefined) {
@@ -23,32 +66,23 @@ describe("verbal-learning", () => {
             }
             return false;
         };
-        expect(timelineHasRelevantData(timeline)).toBe(true);
-
-        // TODO find a way to mock the audio-keyboard-response plugin
-        // so that the below doesn't complain about failure to load audio files
-        // let timeline = (new VerbalLearning()).getTimeline();
-        // // drop the preload step - jest doesn't play nicely with it
-        // timeline = timeline.slice(1);
-        // expect(timeline.length).toBe(40);
-        // jsPsych.init({timeline: timeline});
-
-        // for (let i = 0; i < timeline.length; i++) {
-        //     const trial = jsPsych.currentTrial();
-        //     if (trial.type === 'html-button-response')  {
-        //         clickContinue();
-        //     } else if (trial.type === 'memory-field') {
-        //         clickContinue("#jspsych-memory-field-button")
-        //     } else if (trial.type === 'audio-keyboard-response') {
-        //         jsPsych.finishTrial();
-        //     } else {
-        //         console.error(trial);
-        //         throw new Error(`Unexpected trial type '${trial.type}'.`);
-        //     }
-        // }
-
-        // // check the data
-        // const relevantData = jsPsych.data.get().filter({isRelevant: true}).values();
-        // expect(relevantData.length).toBeGreaterThan(0);
+        expect(timelineHasRelevantData(timeline1)).toBe(true);
+        expect(timelineHasRelevantData(timeline2)).toBe(true);
+        // test recorded data for relevant data
+        const recordRelevantDataFromTimeline = timeline => {
+            let finished = false;
+            jsPsych.init({
+                timeline: timeline,
+                on_finish: () => { finished = true; },
+            });
+            for (let trials = 0; !finished; ++trials) {
+                if (trials > 100) { throw new Error("too many trials"); }
+                completeCurrentTrial();
+            }
+            return jsPsych.data.get().filter({isRelevant: true}).values();
+            expect(relevant.length).toBeGreaterThan(0);
+        };
+        expect(recordRelevantDataFromTimeline(timeline1).length).toBeGreaterThan(0);
+        expect(recordRelevantDataFromTimeline(timeline2).length).toBeGreaterThan(0);
     });
 });
