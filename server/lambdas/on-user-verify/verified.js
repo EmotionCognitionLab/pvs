54e02dc17416ca23c94b25817eb2290fc40581b8
Floozutter <me@floozutter.site>
2021-12-22 18:34:48 -0800

    define short-id-generating func by loading words

--- server/lambdas/on-user-verify/verified.js
+++ server/lambdas/on-user-verify/verified.js
@@ -6,10 +6,18 @@
  **/
 
 const AWS = require('aws-sdk');
+const fs = require("fs");
+
 const region = process.env.REGION;
 const usersTable = process.env.USERS_TABLE;
 const dynamoEndpoint = process.env.DYNAMO_ENDPOINT;
 const dynamo = new AWS.DynamoDB.DocumentClient({endpoint: dynamoEndpoint, apiVersion: '2012-08-10', region: region});
+
+// filtered WordNet 3.1 words
+const adjs3 = fs.readFileSync("wn3.1-adjs3.txt", "utf8").trim().split("\n");
+const adjs4 = fs.readFileSync("wn3.1-adjs4.txt", "utf8").trim().split("\n");
+const nouns3 = fs.readFileSync("wn3.1-nouns3.txt", "utf8").trim().split("\n");
+const nouns4 = fs.readFileSync("wn3.1-nouns4.txt", "utf8").trim().split("\n");
 
 exports.handler = async (event) => {
     const userRec = buildUserRecord(event);
@@ -21,6 +29,20 @@ exports.handler = async (event) => {
         throw new Error('Something went wrong. Please try again later.') // NB this will be seen by the user
     }
 };
+
+function capitalizeFirst(s) {
+    return s.charAt(0).toUpperCase() + s.slice(1);
+}
+function randomPhrase7() {
+    // choose any adjective
+    const i = Math.floor((adjs3.length+adjs4.length) * Math.random());
+    const adj = i < adjs3.length ? adjs3[i] : adjs4[i-adjs3.length];
+    // choose a noun from only those that will result in a phrase with length 7
+    const validNouns = adj.length === 3 ? nouns4 : nouns3;
+    const j = Math.floor(validNouns.length * Math.random())
+    const noun = validNouns[j];
+    return capitalizeFirst(adj) + capitalizeFirst(noun);
+}
 
 function buildUserRecord(event) {
     let result = {
