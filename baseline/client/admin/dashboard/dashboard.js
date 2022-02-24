1c619a10820b1f8ed7075ca3b45320093910b37f
Noah Mercer <noah.mercer@gmail.com>
2022-02-23 21:06:09 -0800

    Adds rudimentary user info display

--- baseline/client/admin/dashboard/dashboard.js
+++ baseline/client/admin/dashboard/dashboard.js
@@ -13,60 +13,94 @@ class Dashboard {
         this.allUsersLoadedSuccessfully = false;
     }
 
+    handleCheckboxEvent(event) {
+        const checkbox = event.target;
+        event.preventDefault();
+        const span = checkbox.labels[0]?.querySelector("span");
+        const key = checkbox.dataset.key;
+        const userId = checkbox.closest("tr")?.dataset.userId;
+        if (!span || !key || !userId) {
+            throw new Error("malformed dashboard table body");
+        }
+        // run logic after event is fully canceled (to make checkbox state reasonable)
+        setTimeout(async () => {
+            if (checkbox.indeterminate) {
+                return;
+            } else if (!checkbox.checked) {
+                Dashboard.disableMarkable(checkbox, span);
+                try {
+                    const user = await this.refreshUser(userId);
+                    const progress = user.progress ?? {};
+                    if (!progress[key]) {
+                        // timestamp for key can be set
+                        progress[key] = (new Date()).toISOString();
+                        await this.client.updateUser(userId, {progress});
+                        this.records.get(userId).user.progress = progress;
+                    }
+                    Dashboard.setMarkable(checkbox, span, progress[key]);
+                } catch (err) {
+                    console.error(`Error setting date for ${key} for ${userId}`, err);
+                    window.alert("A problem occurred. Please try again later.");
+                    Dashboard.clearMarkable(checkbox, span);
+                }
+            } else if (window.confirm("Unset this timestamp?")) {
+                const timestamp = span.textContent;
+                Dashboard.disableMarkable(checkbox, span);
+                try {
+                    const user = await this.refreshUser(userId);
+                    const progress = user.progress ?? {};
+                    if (progress[key]) {
+                        delete progress[key];
+                        await this.client.updateUser(userId, {progress});
+                        this.records.get(userId).user.progress = progress;
+                    }
+                    Dashboard.clearMarkable(checkbox, span);
+                } catch (err) {
+                    console.error(`Error clearing date for ${key} for ${userId}`, err);
+                    window.alert("A problem occurred. Please try again later.");
+                    Dashboard.setMarkable(checkbox, span, timestamp);
+                }
+            }
+        });
+    }
+
+    handleUserEvent(event) {
+        event.stopPropagation();
+        const parentRow = event.target.closest("[data-user-id]");
+        const userId = parentRow.dataset.userId;
+        const user = this.records.get(userId).user;
+        const userDetails = [user.email, user.phone_number, user.userId];
+        const divs = userDetails.map(item => {
+            const elem = document.createElement("div");
+            elem.textContent = item;
+            return elem;
+        });
+        const deetsDiv = Dashboard.getUserDetailsDiv();
+        while (deetsDiv.hasChildNodes()) {
+            deetsDiv.removeChild(deetsDiv.lastChild);
+        }
+        divs.forEach(div => deetsDiv.appendChild(div));
+        deetsDiv.classList.remove("hidden");
+    }
+
     listen() {
         // add checkbox click event listener to table body
         this.tbody.addEventListener("click", event => {
-            const checkbox = event.target;
-            if (checkbox.type !== "checkbox") {
-                return;
+            const target = event.target;
+            if (target.type == "checkbox") {
+                this.handleCheckboxEvent(event);
+            } else if (target.className == "username") {
+                this.handleUserEvent(event);
             }
-            event.preventDefault();
-            const span = checkbox.labels[0]?.querySelector("span");
-            const key = checkbox.dataset.key;
-            const userId = checkbox.closest("tr")?.dataset.userId;
-            if (!span || !key || !userId) {
-                throw new Error("malformed dashboard table body");
+            return;
+        });
+
+        // hides the user details div when you click anywhere outside of it
+        document.addEventListener("click", event => {
+            const deetsDiv = Dashboard.getUserDetailsDiv();
+            if (event.target !== deetsDiv && event.target.parentNode !== deetsDiv && !deetsDiv.classList.contains("hidden")) {
+                deetsDiv.classList.add("hidden");
             }
-            // run logic after event is fully canceled (to make checkbox state reasonable)
-            setTimeout(async () => {
-                if (checkbox.indeterminate) {
-                    return;
-                } else if (!checkbox.checked) {
-                    Dashboard.disableMarkable(checkbox, span);
-                    try {
-                        const user = await this.refreshUser(userId);
-                        const progress = user.progress ?? {};
-                        if (!progress[key]) {
-                            // timestamp for key can be set
-                            progress[key] = (new Date()).toISOString();
-                            await this.client.updateUser(userId, {progress});
-                            this.records.get(userId).user.progress = progress;
-                        }
-                        Dashboard.setMarkable(checkbox, span, progress[key]);
-                    } catch (err) {
-                        console.error(`Error setting date for ${key} for ${userId}`, err);
-                        window.alert("A problem occurred. Please try again later.");
-                        Dashboard.clearMarkable(checkbox, span);
-                    }
-                } else if (window.confirm("Unset this timestamp?")) {
-                    const timestamp = span.textContent;
-                    Dashboard.disableMarkable(checkbox, span);
-                    try {
-                        const user = await this.refreshUser(userId);
-                        const progress = user.progress ?? {};
-                        if (progress[key]) {
-                            delete progress[key];
-                            await this.client.updateUser(userId, {progress});
-                            this.records.get(userId).user.progress = progress;
-                        }
-                        Dashboard.clearMarkable(checkbox, span);
-                    } catch (err) {
-                        console.error(`Error clearing date for ${key} for ${userId}`, err);
-                        window.alert("A problem occurred. Please try again later.");
-                        Dashboard.setMarkable(checkbox, span, timestamp);
-                    }
-                }
-            });
         });
     }
 
@@ -153,6 +187,10 @@ class Dashboard {
         return label;
     }
 
+    static getUserDetailsDiv() {
+        return document.getElementById("user-details");
+    }
+
     appendRow(userId) {
         // prepare data
         const {user, finishedSetsT1, finishedSetsT2, finishedSessions} = this.records.get(userId);
@@ -162,11 +200,14 @@ class Dashboard {
         row.dataset.userId = userId;
         // add Subject ID cell
         const subjectCell = row.insertCell();
-        subjectCell.textContent = user.name;
-        const dateSpan = document.createElement("span");
-        dateSpan.textContent = user.createdAt.substring(0, 10);
-        dateSpan.classList.add("small");
-        subjectCell.appendChild(dateSpan);
+        const nameDiv = document.createElement("div");
+        nameDiv.textContent = user.name;
+        nameDiv.classList.add("username");
+        subjectCell.append(nameDiv);
+        const dateDiv = document.createElement("div");
+        dateDiv.textContent = user.createdAt.substring(0, 10);
+        dateDiv.classList.add("small");
+        subjectCell.appendChild(dateDiv);
         // Daily Tasks T1
         row.insertCell().appendChild(Dashboard.createProgress(6, finishedSetsT1, "sets"));
         // EEG T1
@@ -181,6 +222,14 @@ class Dashboard {
         row.insertCell().appendChild(Dashboard.createMarkable(user.progress, "mriT2"));
         // Daily Tasks T2
         row.insertCell().appendChild(Dashboard.createProgress(6, finishedSetsT2, "sets"));
+    }
+
+    showUserDetails(userId) {
+        const user = this.records.get(userId);
+        const details = JSON.stringify(user);
+        const deetsDiv = document.getElementById("user-details");
+        deetsDiv.textContent = details;
+        deetsDiv.classList.remove("hidden");
     }
 
     clearRows() {
