3a4cdabcbfc695d6747731ccae3d4044ea2a6306
Noah Mercer <noah.mercer@gmail.com>
2022-03-09 11:18:17 -0800

    Adds initial ds oauth call

--- baseline/client/admin/docusign/docusign.js
+++ baseline/client/admin/docusign/docusign.js
@@ -1,9 +1,17 @@
 import { getAuth } from "auth/auth.js";
 import ApiClient from "../../api/client";
+import awsSettings from '../../../../common/aws-settings.json';
 
 class Consent {
     constructor(session) {
         this.client = new ApiClient(session);
+        this.client.getSelf().then(user => {
+            if (!user.dsAuthed) {
+                // do the OAuth dance to get the Docusign info
+                const oauthUri = Consent.getOAuthUrl(["extended", "signature"]);
+                window.location.href = oauthUri;
+            }
+        });
     }
 
     handleFormSubmission(event) {
@@ -12,6 +20,13 @@ class Consent {
         console.debug(Consent.getInputValue(Consent.emailId));
     }
 }
+
+Consent.getOAuthUrl = (scopes) => {
+    const dsClientId = awsSettings.DsId;
+    const dsRedirectUri = encodeURIComponent(awsSettings.DsRedirectUri);
+    const dsOAuthUri = awsSettings.DsOAuthUri;
+    return `${dsOAuthUri}?response_type=code&scope=${scopes.join('+')}&client_id=${dsClientId}&redirect_uri=${dsRedirectUri}`;
+};
 
 Consent.getInputValue = (elemId) => {
     return document.getElementById(elemId).value;
