e6925a39f0a13bc4c9c26bd7fdb8ca2c2ea2a017
Noah Mercer <noah.mercer@gmail.com>
2022-03-17 15:07:45 -0700

    Converts SetupComponent to script setup style.

--- client/src/components/SetupComponent.vue
+++ client/src/components/SetupComponent.vue
@@ -1,33 +1,20 @@
 <template>
-    <div v-if="currentlyLoggedIn"><condition-assignment-component></condition-assignment-component></div>
+    <div v-if="currentlyLoggedIn"><ConditionAssignmentComponent /></div>
     <div v-else>
         <br/>
-        <LoginComponent></LoginComponent>
+        <LoginComponent />
     </div>
 </template>
-<script>
-import { ref, toRefs } from '@vue/runtime-core';
-import { ipcRenderer } from 'electron'
-import LoginComponent from './LoginComponent.vue'
-import ConditionAssignmentComponent from './ConditionAssignmentComponent.vue'
-// import SubjectNumberComponent from './SubjectNumberComponent.vue'
 
-export default {
-    name: 'SetupComponent',
-    components: { LoginComponent, ConditionAssignmentComponent },
-    props: {
-        loggedIn: {
-            type: Boolean,
-            required: true
-        }
-    },
-    setup(props) {
-        const { loggedIn } = toRefs(props)
-        let currentlyLoggedIn = ref(loggedIn.value)
-        ipcRenderer.on('login-succeeded', () => {
-            currentlyLoggedIn.value = true
-        })
-        return { currentlyLoggedIn }
-    },
-}
+<script setup>
+    import { ref } from '@vue/runtime-core';
+    import { ipcRenderer } from 'electron'
+    import LoginComponent from './LoginComponent.vue'
+    import ConditionAssignmentComponent from './ConditionAssignmentComponent.vue'
+
+    const loggedIn = ref(true)
+    let currentlyLoggedIn = ref(loggedIn.value)
+    ipcRenderer.on('login-succeeded', () => {
+        currentlyLoggedIn.value = true
+    })
 </script>
