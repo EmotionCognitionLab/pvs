b935deee66d1910d9f261037bfb74dce9998ec21
Noah Mercer <noah.mercer@gmail.com>
2022-04-05 13:26:09 -0700

    Adds assignment to condition endpoint for user api

--- server/lambdas/api/api.js
+++ server/lambdas/api/api.js
@@ -4,6 +4,15 @@ const usersTable = process.env.USERS_TABLE;
 const dynamoEndpoint = process.env.DYNAMO_ENDPOINT;
 const docClient = new AWS.DynamoDB.DocumentClient({endpoint: dynamoEndpoint, apiVersion: "2012-08-10", region: region});
 
+// For assignment to condition participants are asked their birth sex,
+// and, iff the answer is 'Intersex', how they describe their sex.
+const validSex = ['Male', 'Female', 'Intersex'];
+const validSexDesc = ['Male', 'Female', 'Other'];
+
+// The possible conditions a user can be assigned to
+// exported for testing
+export const validConditions = ['A', 'B'];
+
 exports.handler = async (event) => {
     const path = event.requestContext.http.path;
     const method = event.requestContext.http.method;
@@ -14,6 +23,9 @@ exports.handler = async (event) => {
         if (method === "PUT") {
             return updateSelf(event.requestContext.authorizer.jwt.claims.sub, JSON.parse(event.body));
         }
+    }
+    if (path === "/condition" && method === "POST") {
+        return assignToCondition(event.requestContext.authorizer.jwt.claims.sub, JSON.parse(event.body))
     }
     return errorResponse({statusCode: 400, message: `Unknown operation "${method} ${path}"`});
 }
@@ -73,6 +85,100 @@ const updateSelf = async(userId, updates) => {
         };
         await docClient.update(params).promise();
         return successResponse({msg: "update successful"});
+    } catch (err) {
+        console.error(err);
+        if (!(err instanceof HttpError)) {
+            err = new HttpError(err.message);
+        }
+        return errorResponse(err);
+    }
+}
+
+const assignToCondition = async(userId, data) => {
+    const bornSex = data['bornSex'];
+    const sexDesc = data['sexDesc'] ? data['sexDesc'] : '';
+    if (!validSex.includes(bornSex)) {
+        return errorResponse({
+            message: `${bornSex} is not a valid option.`,
+            statusCode: 400
+        });
+    }
+    if (bornSex === 'Intersex' && !validSexDesc.includes(sexDesc)) {
+        return errorResponse({
+            message: `${sexDesc} is not a valid option.`,
+            statusCode: 400
+        });
+    }
+    
+    // per the spec, assign sex to be Male if they were born
+    // Intersex and identify as Other
+    let assignedSex;
+    if (bornSex !== 'Intersex') {
+        assignedSex = bornSex;
+    } else if (sexDesc === 'Other') {
+        assignedSex = 'Male'
+    } else {
+        assignedSex = sexDesc;
+    }
+
+    // get all the users (we expect max # of total users to be ~200)
+    // and filter for those with the same assignedSex
+    try {
+        const params = {
+            TableName: usersTable,
+            FilterExpression: '#condition.assignedSex = :as',
+            ExpressionAttributeNames: {'#condition': 'condition' },
+            ExpressionAttributeValues: {':as': assignedSex }
+        };
+
+        const result = await docClient.scan(params).promise();
+        let condition;
+        if (result.Count % 2 === 0) {
+            // randomly assign to condition
+            if (Math.random() <= 0.5) {
+                condition = validConditions[0];
+            } else {
+                condition = validConditions[1];
+            }
+
+        } else {
+            // sort results by assignedDate and assign to the opposite of
+            // the most recently assigned person
+            result.Items.sort((a, b) => {
+                if (a.condition.assignedDate > b.condition.assignedDate) return -1;
+                if (a.condition.assignedDate < b.condition.assignedDate) return 1;
+                return 0;
+            });
+            const lastAssigned = result.Items[0];
+            if (lastAssigned.condition.assigned === validConditions[0]) {
+                condition = validConditions[1];
+            } else if (lastAssigned.condition.assigned === validConditions[1]) {
+                condition = validConditions[0]
+            } else {
+                return errorResponse({
+                    message: `Unexpected condition '${lastAssigned.condition.assigned}' found; unable to assign ${userId} to condition`,
+                    statusCode: 500
+                });
+            }
+        }
+
+        // save the data
+        const conditionData = {
+            bornSex: bornSex,
+            sexDesc: sexDesc,
+            assignedSex: assignedSex,
+            assigned: condition,
+            assignedDate: new Date().toISOString()
+        };
+        const conditionParams = {
+            TableName: usersTable,
+            Key: { userId: userId },
+            UpdateExpression: 'set #condition = :condition',
+            ExpressionAttributeNames: { '#condition': 'condition' },
+            ExpressionAttributeValues: { ':condition': conditionData }
+        };
+        await docClient.update(conditionParams).promise();
+        return successResponse({condition: condition});
     } catch (err) {
         console.error(err);
         if (!(err instanceof HttpError)) {
