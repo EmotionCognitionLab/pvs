f40001b0d2c073e79c6f63264b861059e0172888
Noah Mercer <noah.mercer@gmail.com>
2022-04-08 13:49:18 -0700

    Cleans up App.vue and moves it to script setup

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,37 +1,8 @@
 <template>
- <!-- 
-  <TimerComponent v-bind:secondsDuration=300 @timerStarted="handleTimerStart" @timerStopped="handleTimerStop"></TimerComponent>
-  <br/>
- <em-wave-listener ref="emWaveListener"></em-wave-listener> 
-  <br/>
-  <web-page-component source="https://wikipedia.org"></web-page-component>
-  -->
     <router-view></router-view>
 </template>
 
-<script>
-// import TimerComponent from './components/TimerComponent.vue'
-// import EmWaveListener from './components/EmWaveListener.vue'
-// import { ref } from '@vue/runtime-core'
-
-export default {
-  name: 'App',
-  components: {
-    // EmWaveListener,
-    // TimerComponent
-  },
-  setup() {
-    // const emWaveListener = ref(null)
-    // const handleTimerStart = () => {
-    //   emWaveListener.value.startPulseSensor()
-    // }
-    // const handleTimerStop = () => {
-    //   emWaveListener.value.stopPulseSensor()
-    // }
-
-    // return {emWaveListener, handleTimerStart, handleTimerStop}
-  }
-}
+<script setup>
 </script>
 
 <style>
