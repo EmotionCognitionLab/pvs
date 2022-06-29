b98cb841738d3d82bb31afee2a70e7741c9856a3
Noah Mercer <noah.mercer@gmail.com>
2022-06-28 21:01:35 -0700

    Slightly generalizes RestComponent

--- client/src/components/RestComponent.vue
+++ client/src/components/RestComponent.vue
@@ -1,14 +1,18 @@
 <template>
     <div class="instruction" v-if="!done">
-        When you are ready to rest for 5 minutes while measuring your heart rate, please connect your pulse sensor to your ear and to the computer and press the start button.
+        <slot name="preText">
+            When you are ready to rest for 5 minutes while measuring your heart rate, please connect your pulse sensor to your ear and to the computer and press the start button.
+        </slot>
         <EmWaveListener :showIbi=false @pulseSensorCalibrated="startTimer" @pulseSensorStopped="stopTimer" @pulseSensorSignalLost="stopTimer" @pulseSensorSignalRestored="startTimer" ref="emwaveListener"/> 
         <br/>
         <TimerComponent :secondsDuration=300 :showButtons=false @timerFinished="stopSession" ref="timer" />
     </div>
     <div class="instruction" v-else>
-        All done for today! Please come back tomorrow for more training.
-        <br/>
-        <button class="button"  @click="quit">Quit</button>
+        <slot name="postText">
+            All done for today! Please come back tomorrow for more training.
+            <br/>
+            <button class="button"  @click="quit">Quit</button>
+        </slot>
     </div>
 </template>
 <script setup>
@@ -20,6 +24,7 @@ import TimerComponent from './TimerComponent.vue'
 const emwaveListener = ref(null)
 const timer = ref(null)
 const done = ref(false)
+const emit = defineEmits(['timer-finished'])
 
 function startTimer() {
     timer.value.running = true
@@ -32,6 +37,7 @@ function stopTimer() {
 function stopSession() {
     emwaveListener.value.stopSensor = true
     done.value = true
+    emit('timer-finished')
 }
 
 function quit() {
