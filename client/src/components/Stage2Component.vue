29bd39c9a75722619c6e480177ec9fd6b8e1b6e5
Noah Mercer <noah.mercer@gmail.com>
2022-06-29 14:33:34 -0700

    Adds data upload to stage 2

--- client/src/components/Stage2Component.vue
+++ client/src/components/Stage2Component.vue
@@ -2,19 +2,34 @@
     <div class="instruction" v-if="windowReady && !lumosityDone">
         <LumosityComponent @lumosityFinished="completedLumosity"/>
     </div>
-    <div class="instruction" v-else>
-        <RestComponent />
+    <div class="instruction" v-else-if="lumosityDone && !restBreathingDone">
+        <RestComponent @timerFinished="restBreathingDone=true" />
+    </div>
+    <div v-else-if="lumosityDone && restBreathingDone">
+        <UploadComponent>
+            <template #preUploadText>
+                <div class="instruction">Terrific! Please wait while we upload your data...</div>
+            </template>
+            <template #postUploadText>
+                    <div class="instruction">Upload complete! Please come back tomorrow for more training.</div>
+                <br/>
+                <button class="button" @click="quit">Quit</button>
+            </template>
+        </UploadComponent>
     </div>
 </template>
 <script setup>
+import { ipcRenderer } from 'electron'
 import { ref, onMounted } from '@vue/runtime-core'
 import LumosityComponent from './LumosityComponent.vue'
 import RestComponent from './RestComponent.vue'
+import UploadComponent from './UploadComponent.vue'
 import ApiClient from '../../../common/api/client.js'
 import { SessionStore } from '../session-store.js'
 
 const lumosityDone = ref(false)
 const windowReady = ref(false)
+const restBreathingDone = ref(false)
 let session
 let apiClient
 let lumosDays = []
@@ -36,6 +51,10 @@ async function completedLumosity() {
     const now = new Date()
     lumosDays.push(`${now.getFullYear()}${(now.getMonth() + 1).toString().padStart(2,0)}${now.getDate().toString().padStart(2, 0)}`)
     await apiClient.updateSelf({lumosDays: lumosDays})
+}
+
+function quit() {
+    ipcRenderer.invoke('quit')
 }
 
 </script>
