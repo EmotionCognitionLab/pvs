a66d73cc3261ef8f35831a4e85d1dc09158f9744
Noah Mercer <noah.mercer@gmail.com>
2022-07-01 13:07:04 -0700

    Adds upload to stage 3.

--- client/src/components/BreathComponent.vue
+++ client/src/components/BreathComponent.vue
@@ -1,20 +1,27 @@
 <template>
     <div>
         <div class="instruction" v-if="!sessionDone && !dayDone">
-        We have one remaining task for you today.
-        Please breathe following the ball on the screen.
-        Breathe in while the ball is moving up and breathe out while the ball is moving down.
-        Make sure you have the pulse device attached to your ear, and click "Start" when you're ready to begin.
+            We have one remaining task for you today.
+            Please breathe following the ball on the screen.
+            Breathe in while the ball is moving up and breathe out while the ball is moving down.
+            Make sure you have the pulse device attached to your ear, and click "Start" when you're ready to begin.
         </div>
         <div class="instruction" v-else-if="sessionDone && !dayDone">
-        All done! Please come back later today to do your next session.
-        <br/>
-        <button class="button" @click="sessionDone=false">Start Next Session</button>
+            All done! Please come back later today to do your next session.
+            <br/>
+            <button class="button" @click="sessionDone=false">Start Next Session</button>
         </div>
-        <div class="instruction" v-else>
-        All done for today! Please come back tomorrow for more training.
-        <br/>
-        <button class="button"  @click="quit">Quit</button>
+        <div class="instruction" v-else-if="sessionDone && dayDone">
+             <UploadComponent>
+                <template #preUploadText>
+                    <div class="instruction">Terrific! Please wait while we upload your data...</div>
+                </template>
+                <template #postUploadText>
+                        <div class="instruction">Upload complete. You're all done for today! Please come back tomorrow for more training.</div>
+                    <br/>
+                    <button class="button" @click="quit">Quit</button>
+                </template>
+            </UploadComponent>
         </div>
         <div v-if="!sessionDone && !dayDone && regimes && regimes.length"> <!-- Need regimes && regimes.length here to prevent render before regimes has been initialized -->
             <PacerComponent 
@@ -35,6 +42,7 @@ import { ref, onBeforeMount, computed } from '@vue/runtime-core'
 import ApiClient from '../../../common/api/client.js'
 import { SessionStore } from '../session-store.js'
 import PacerComponent from './PacerComponent.vue'
+import UploadComponent from './UploadComponent.vue'
 import EmWaveListener from './EmWaveListener.vue'
 
 const pacer = ref(null)
