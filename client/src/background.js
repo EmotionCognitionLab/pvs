f881ed8c76ec07f795524cded2d3962b7b8d4412
Noah Mercer <noah.mercer@gmail.com>
2022-07-12 06:53:16 -0700

    Dev tools no longer needed in prod

--- client/src/background.js
+++ client/src/background.js
@@ -54,7 +54,6 @@ async function createWindow() {
     createProtocol('app')
     // Load the index.html when not in development
     win.loadURL('app://./index.html')
-    win.webContents.openDevTools()
   }
   return win
 }
