9f46687bd02775eaa3666c9e415c7b6a5ecaf443
Noah Mercer <noah.mercer@gmail.com>
2022-08-29 20:07:30 -0700

    #227 Adds "all done for today" component

---
+++ client/src/components/DoneTodayComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <div class="instruction">
+            You're all done for today. Please come back tomorrow to continue!
+        </div>
+        <br/>
+        <button class="button" @click="quit">Quit</button>
+    </div>
+</template>
+<script setup>
+import { ipcRenderer } from 'electron'
+
+function quit() {
+    ipcRenderer.invoke('quit')
+}
+
+</script>
