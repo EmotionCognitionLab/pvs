574f8865a2af0fad70f555c18e2e34fd780468a7
Floozutter <me@floozutter.site>
2022-09-28 14:52:01 -0700

    schedule panas for sets 1, 3, 5

--- baseline/client/daily-tasks/daily-tasks.js
+++ baseline/client/daily-tasks/daily-tasks.js
@@ -32,11 +32,11 @@ import version from "../version.json";
  */
 
 const set1 = ["mood-prediction", "panas", "daily-stressors", "dass", "n-back", "mind-in-eyes", "verbal-fluency", "flanker", "face-name", "spatial-orientation"];
-const set2 = ["demographics", "physical-activity", "panas", "daily-stressors", "verbal-fluency", "n-back", "pattern-separation-learning", "face-name", "spatial-orientation", "mind-in-eyes", "pattern-separation-recall"];
+const set2 = ["demographics", "physical-activity", "daily-stressors", "verbal-fluency", "n-back", "pattern-separation-learning", "face-name", "spatial-orientation", "mind-in-eyes", "pattern-separation-recall"];
 const set3 = ["panas", "daily-stressors", "task-switching", "mind-in-eyes", "verbal-fluency", "face-name", "n-back", "spatial-orientation", "flanker"];
-const set4 = ["panas", "daily-stressors", "ffmq", "pattern-separation-learning", "spatial-orientation", "verbal-fluency", "n-back", "mind-in-eyes", "face-name", "pattern-separation-recall"];
-const set5 = ["verbal-learning-learning", "face-name", "n-back", "mind-in-eyes", "flanker", "verbal-learning-recall"];
-const set6 = ["mood-memory", "panas", "daily-stressors", "pattern-separation-learning", "n-back", "verbal-fluency", "spatial-orientation", "mind-in-eyes", "face-name", "pattern-separation-recall"];
+const set4 = ["daily-stressors", "ffmq", "pattern-separation-learning", "spatial-orientation", "verbal-fluency", "n-back", "mind-in-eyes", "face-name", "pattern-separation-recall"];
+const set5 = ["verbal-learning-learning", "face-name", "n-back", "mind-in-eyes", "flanker", "panas", "verbal-learning-recall"];
+const set6 = ["mood-memory", "daily-stressors", "pattern-separation-learning", "n-back", "verbal-fluency", "spatial-orientation", "mind-in-eyes", "face-name", "pattern-separation-recall"];
 const allSets = [set1, set2, set3, set4, set5, set6];
 const setFinished = "set-finished";
 const setStarted = "set-started";
