af0b4aae8bb03d093a48a315f925d02e013759cf
Noah Mercer <noah.mercer@gmail.com>
2022-09-29 17:13:11 -0700

    Adds video for start of stage 2.

--- client/src/components/Stage2Component.vue
+++ client/src/components/Stage2Component.vue
@@ -1,26 +1,35 @@
 <template>
-    <div v-if="lumosDataReady">
-        <div class="instruction-small" v-if="!lumosityDone">
-            <LumosityComponent @lumosityFinished="finishedLumosity()"/>
-        </div>
-        <div class="instruction" v-else-if="lumosityDone && !restBreathingDone">
-            <RestComponent @timerFinished="restBreathingDone=true" />
-        </div>
-        <div v-else-if="lumosityDone && restBreathingDone">
-            <UploadComponent>
-                <template #preUploadText>
-                    <div class="instruction">Terrific! Please wait while we upload your data...</div>
-                </template>
-                <template #postUploadText>
-                        <div class="instruction">Upload complete! Please come back tomorrow for more training.</div>
-                    <br/>
-                    <button class="button" @click="quit">Quit</button>
-                </template>
-            </UploadComponent>
-        </div>
+    <div v-if="firstTime" class="instruction">
+        In this video, Dr. Mara Mather explains the next phase of the study.
+        <br/>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/MAVUImKD36E" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <br/>
+        <button @click="continueToLumosity" class="button">Continue</button>
     </div>
-    <div class="instruction" v-else>
-        One moment while we load your data...
+    <div v-else>
+        <div v-if="lumosDataReady">
+            <div class="instruction-small" v-if="!lumosityDone">
+                <LumosityComponent @lumosityFinished="finishedLumosity()"/>
+            </div>
+            <div class="instruction" v-else-if="lumosityDone && !restBreathingDone">
+                <RestComponent @timerFinished="restBreathingDone=true" />
+            </div>
+            <div v-else-if="lumosityDone && restBreathingDone">
+                <UploadComponent>
+                    <template #preUploadText>
+                        <div class="instruction">Terrific! Please wait while we upload your data...</div>
+                    </template>
+                    <template #postUploadText>
+                            <div class="instruction">Upload complete! Please come back tomorrow for more training.</div>
+                        <br/>
+                        <button class="button" @click="quit">Quit</button>
+                    </template>
+                </UploadComponent>
+            </div>
+        </div>
+        <div class="instruction" v-else>
+            One moment while we load your data...
+        </div>
     </div>
 </template>
 <script setup>
@@ -36,6 +45,7 @@ const restBreathingDone = ref(false)
 const lumosDays = ref(null)
 const lumosityDone = ref(null)
 const lumosDataReady = ref(null)
+const firstTime = ref(window.localStorage.getItem('HeartBeam.hasSeenStage2Vid') !== 'true')
 
 onBeforeMount(async() => {
      const { days, done, ready } = await useLumosityHelper()
@@ -47,6 +57,11 @@ onBeforeMount(async() => {
     const todayYYMMDD = yyyymmddNumber(new Date())
     restBreathingDone.value = restBreathingDays.has(todayYYMMDD)
 })
+
+function continueToLumosity() {
+    window.localStorage.setItem('HeartBeam.hasSeenStage2Vid', 'true')
+    firstTime.value = false
+}
 
 function quit() {
     ipcRenderer.invoke('quit')
