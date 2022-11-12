31e6a575be5a5f9298e1acd4b388f7bdb3b599dd
Noah Mercer <noah.mercer@gmail.com>
2022-11-11 16:45:12 -0800

    Give pay-info build process so electron can use it

---
+++ common/pay-info/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path")
+
+module.exports = {
+  entry: path.resolve(__dirname, "pay-info.js"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "payinfo.bundle.js",
+    library: {
+        name: "payInfo",
+        type: "umd"
+    },
+  },
+  module: {
+    rules: [
+        {
+            test: /\.(js)$/,
+            exclude: /node_modules/,
+            use: "babel-loader",
+        },
+        {
+            test: /\.html$/i,
+            loader: "html-loader",
+        },
+    ],
+  },
+  mode: "production",
+}
