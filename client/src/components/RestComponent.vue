f6f50f0829503c029da5770a08c1f3ec0a96805b
Noah Mercer <noah.mercer@gmail.com>
2022-12-01 15:52:27 -0800

    #276 Minor RestComponent user instruction change

--- client/src/components/RestComponent.vue
+++ client/src/components/RestComponent.vue
@@ -2,7 +2,7 @@
     <div class="instruction" v-if="!done">
         <slot name="preText">
             When you are ready to rest for 5 minutes while measuring your heart rate, please connect your pulse sensor to your ear and to the computer and press the start button.
-            Please remember to sit on a chair with your feet flat on the floor and hands resting on your legs during this portion.
+            Please remember to sit on a chair with your feet flat on the floor and hands resting on your legs. Please rest and do not do anything during this portion.
         </slot>
         <EmWaveListener :showIbi=false @pulseSensorCalibrated="startTimer" @pulseSensorStopped="sensorStopped" @pulseSensorSignalLost="sensorStopped" @pulseSensorSignalRestored="startTimer" @pulseSensorSessionEnded="resetTimer" ref="emwaveListener"/> 
         <br/>
