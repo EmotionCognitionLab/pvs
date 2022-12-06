f0033dd16d43f372be4daf333b74e1dbb2f0e04b
Noah Mercer <noah.mercer@gmail.com>
2022-12-05 19:47:06 -0800

    #96 Shows earnings data in payboard

--- common/pay-info/pay-info.js
+++ common/pay-info/pay-info.js
@@ -1,4 +1,5 @@
 import payboardTempl from "./payboard.handlebars";
+import { earningsTypes } from "../types/types.js";
 
 export class Payboard {
     constructor(rootDiv, errorDiv, client, userId, admin = false) {
@@ -43,28 +44,56 @@ export class Payboard {
         try {
             // get data
             let user;
+            let earnings;
             if (this.admin) {
                 user = await this.client.getUser(this.userId, true);
+                earnings = await this.client.getEarningsForUser(this.userId);
             } else {
                 user = await this.client.getSelf();
+                earnings = await this.client.getEarningsForSelf();
             }
             const data = {};
             data.admin = this.admin;
-            const visitStatus = (visitDate, dropDate) => {
+
+            const earningsForType = (earningType) => {
+                const earned = earnings.filter(e => e.type === earningType);
+                if (earned.length === 0) return '';
+                // for some types we return the sum of the earnings
+                if ([
+                        earningsTypes.LUMOS_AND_BREATH_1,
+                        earningsTypes.BREATH2, earningsTypes.LUMOS_BONUS,
+                        earningsTypes.BREATH_BONUS
+                    ].includes(earningType)) {
+                        return earned.reduce((prev, cur) => prev + cur.amount, 0);
+                } else {
+                    if (earned.length > 1) throw new Error(`Expected only one earnings result of type ${earningType} for user ${this.userId}, but found ${earned.length}.`);
+                    return earned[0].amount;
+                }
+            }
+
+            const visitStatus = (whichVisit, visitDate, dropDate) => {
                 if (!visitDate) return {status: "Not Started", earned: ''};
-                if ((visitDate && !dropDate) || (visitDate < dropDate)) return {status: "Completed", earned: "$50"};
+                if ((visitDate && !dropDate) || (visitDate < dropDate)) {
+                    const earned = earningsForType(whichVisit);
+                    return {status: "Completed", earned: earned};
+                }
                 return {status: "Dropped out", earned: ''}
             }            
 
-            data.visit1 = visitStatus(user.progress?.visit1, user.progress?.dropped);
-            data.visit2 = visitStatus(user.progress?.visit2, user.progress?.dropped);
-            data.visit3 = visitStatus(user.progress?.visit3, user.progress?.dropped);
-            data.visit4 = visitStatus(user.progress?.visit4, user.progress?.dropped);
-            data.visit5 = visitStatus(user.progress?.visit5, user.progress?.dropped);
+            data.visit1 = visitStatus("visit1", user.progress?.visit1, user.progress?.dropped);
+            data.visit2 = visitStatus("visit2", user.progress?.visit2, user.progress?.dropped);
+            data.visit3 = visitStatus("visit3", user.progress?.visit3, user.progress?.dropped);
+            data.visit4 = visitStatus("visit4", user.progress?.visit4, user.progress?.dropped);
+            data.visit5 = visitStatus("visit5", user.progress?.visit5, user.progress?.dropped);
             data.week1Status = user.preComplete? 'Completed' : 'In Progress';
-            data.week1Earned = user.preComplete? '$30' : '';
+            data.week1Earned = earningsForType(earningsTypes.PRE);
             data.week12Status = user.postComplete ? 'Completed' : '';
-            data.week12Status = user.postComplete ? '$30' : '';
+            data.week12Earned = earningsForType(earningsTypes.POST);
+            data.lumosityEarned = earningsForType(earningsTypes.LUMOS_AND_BREATH_1);
+            data.breathingEarned = earningsForType(earningsTypes.BREATH2);
+            const lumosBonus = earningsForType(earningsTypes.LUMOS_BONUS);
+            const breathBonus = earningsForType(earningsTypes.BREATH_BONUS);
+            data.bonusEarned = lumosBonus + breathBonus;
             if (user.homeComplete) {
                 data.lumosityStatus = 'Completed';
                 data.breathingStatus = 'Completed';
