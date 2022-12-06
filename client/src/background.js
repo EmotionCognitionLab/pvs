220299eec953698085917562916297600681b352
Noah Mercer <noah.mercer@gmail.com>
2022-12-06 14:55:04 -0800

    Simplifies application menu

--- client/src/background.js
+++ client/src/background.js
@@ -90,12 +90,7 @@ function buildMenuTemplate(window) {
     {
       label: 'Edit',
       submenu: [
-        { role: 'undo' },
-        { role: 'redo' },
-        { type: 'separator' },
-        { role: 'cut' },
         { role: 'copy' },
-        { role: 'paste' },
         ...(isMac ? [
           { role: 'pasteAndMatchStyle' },
           { role: 'delete' },
@@ -108,11 +103,7 @@ function buildMenuTemplate(window) {
               { role: 'stopSpeaking' }
             ]
           }
-        ] : [
-          { role: 'delete' },
-          { type: 'separator' },
-          { role: 'selectAll' }
-        ])
+        ] : [ ])
       ]
     },
     // { role: 'viewMenu' }
@@ -122,10 +113,6 @@ function buildMenuTemplate(window) {
         { role: 'reload' },
         { role: 'forceReload' },
         { role: 'toggleDevTools' },
-        { type: 'separator' },
-        { role: 'resetZoom' },
-        { role: 'zoomIn' },
-        { role: 'zoomOut' },
         { type: 'separator' },
         { role: 'togglefullscreen' },
         { type: 'separator' },
@@ -137,7 +124,6 @@ function buildMenuTemplate(window) {
       label: 'Window',
       submenu: [
         { role: 'minimize' },
-        { role: 'zoom' },
         ...(isMac ? [
           { type: 'separator' },
           { role: 'front' },
@@ -146,18 +132,6 @@ function buildMenuTemplate(window) {
         ] : [
           { role: 'close' }
         ])
-      ]
-    },
-    {
-      role: 'help',
-      submenu: [
-        {
-          label: 'Learn More',
-          click: async () => {
-            const { shell } = require('electron')
-            await shell.openExternal('https://electronjs.org')
-          }
-        }
       ]
     }
   ]
