e080137cdb08b2af3c81e1b0a80d8e1c78fc9b39
Noah Mercer <noah.mercer@gmail.com>
2023-03-09 11:53:34 -0800

    #359 sets preComplete when visit2 checked

--- baseline/client/admin/dashboard/index.js
+++ baseline/client/admin/dashboard/index.js
@@ -2,12 +2,16 @@ import "./style.css";
 import { Dashboard } from "./dashboard.js";
 import { getAuth } from "auth/auth.js";
 import ApiClient from "../../../../common/api/client.js";
+import Db from "../../../../common/db/db.js";
 
 getAuth(
     async session => {
+        const db = new Db();
+        db.session = session;
         const dashboard = new Dashboard(
             document.querySelector("#dashboard > tbody"),
-            new ApiClient(session)
+            new ApiClient(session),
+            db
         );
         await dashboard.refreshRecords();
         dashboard.showActive();
