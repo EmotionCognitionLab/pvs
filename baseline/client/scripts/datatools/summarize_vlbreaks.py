f6d8810fcabf39ad647e4a7831cc0c03e978a04b
Noah Mercer <noah.mercer@gmail.com>
2023-03-22 15:56:12 -0700

    #207 makes intertask interval code more general

--- baseline/client/scripts/datatools/summarize_vlbreaks.py
+++ baseline/client/scripts/datatools/summarize_vlbreaks.py
@@ -6,18 +6,13 @@ from intertask_intervals import IntertaskIntervals
 
 def parse_args() -> tuple[Path, Path]:
     parser = argparse.ArgumentParser()
-    parser.add_argument("vll_json_file", type = Path)
-    parser.add_argument("vlr_json_file", type = Path)
+    parser.add_argument("learning_json_file", type = Path)
+    parser.add_argument("recall_json_file", type = Path)
     args = parser.parse_args()
-    return args.vll_json_file, args.vlr_json_file
+    return args.learning_json_file, args.recall_json_file
 
-def main(vll_json_file: Path, vlr_json_file: Path) -> None:
-    vll_json = json.loads(vll_json_file.read_text("utf-8"))
-    vlr_json = json.loads(vlr_json_file.read_text("utf-8"))
-
-    intervals = IntertaskIntervals()
-
-    for i in vll_json:
+def get_set_and_learning_end_times(learning_json: str, intervals: IntertaskIntervals) -> None:
+    for i in learning_json:
         if not (i.get("userId", False) and i.get("dateTime", False)):
             raise AssertionError
         
@@ -29,7 +24,8 @@ def main(vll_json_file: Path, vlr_json_file: Path) -> None:
             learning_end_time = datetime.fromisoformat(i["dateTime"].replace('Z', ''))
             intervals.set_start_time(i["userId"], learning_end_time)
 
-    for i in vlr_json:
+def get_recall_start_times(recall_json: str, intervals: IntertaskIntervals, recall_filter) -> None:
+    for i in recall_json:
        if not (i.get("userId", False) and i.get("dateTime", False)):
            raise AssertionError
        
@@ -38,9 +34,22 @@ def main(vll_json_file: Path, vlr_json_file: Path) -> None:
            set = i.get("setNum", -1)
            intervals.set_set(i["userId"], set)
            
-       if i.get("stimulus", False) and i["stimulus"].startswith("We presented two different lists of words to you earlier"):
+       if recall_filter(i):
            recall_start_time = datetime.fromisoformat(i["dateTime"].replace('Z', ''))
            intervals.set_end_time(i["userId"], recall_start_time)
+
+def vl_recall_filter(item):
+    return item.get("stimulus", False) and item["stimulus"].startswith("We presented two different lists of words to you earlier")
+
+def main(learning_json_file: Path, recall_json_file: Path) -> None:
+    learning_json = json.loads(learning_json_file.read_text("utf-8"))
+    recall_json = json.loads(recall_json_file.read_text("utf-8"))
+
+    intervals = IntertaskIntervals()
+
+    get_set_and_learning_end_times(learning_json, intervals)
+    
+    get_recall_start_times(recall_json, intervals, vl_recall_filter)
        
     print("user id, set number, seconds from learning end to recall start")
     print(intervals)
